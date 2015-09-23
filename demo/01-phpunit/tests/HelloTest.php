b07dac2e7cede5760a5e319858446861455e4c93
somkiat <somkiat.p@gmail.com>
2015-09-23 23:20:21 +0700

    Add demo to hello phpunit

---
+++ demo/01-phpunit/tests/HelloTest.php
@@ -0,0 +1,12 @@
+<?php
+class HelloTest extends PHPUnit_Framework_TestCase {
+    public function testXXX() {
+        $expectedResult = "Hello, somkiat";
+        $hello = new Hello();
+
+        $actualResult = $hello->sayHi("somkiat");
+
+        $this->assertEquals($expectedResult, $actualResult);
+    }
+}
+?>
