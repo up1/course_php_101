b407fa372438cc59c313ec512e1c0d006ca742db
somkiat <somkiat.p@gmail.com>
2015-09-24 12:21:33 +0700

    Add demo to insert data with PDO

---
+++ demo/02-database/poc_pdo_prepare_insert.php
@@ -0,0 +1,17 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "mypassword";
+$database_name = "sample";
+
+$connection = new PDO("mysql:host=".$host."; dbname=".$database_name."", 
+                                        $username, $password,
+                                        array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+
+$sql = "INSERT INTO USER (firstname, lastname)VALUES (:firstname, :lastname)";
+$statement = $connection->prepare($sql);
+$statement->execute(array(
+                                    ':firstname' => 'somkiat',
+                                    ':lastname' => 'puisungnoen'
+                                    ));
+?>
