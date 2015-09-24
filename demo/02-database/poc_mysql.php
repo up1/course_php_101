2483b160c4309bc24f577f86d40752c0a8dc59a7
somkiat <somkiat.p@gmail.com>
2015-09-24 11:04:17 +0700

    Add demo of php with mysql in procedure way

---
+++ demo/02-database/poc_mysql.php
@@ -0,0 +1,21 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "mypassword";
+$database_name = "sample";
+
+$connection = mysql_connect($host, $username, $password) 
+                                                or die ('Could not connect: '. mysql_error());
+
+mysql_select_db($database_name)
+       or die ('Could not select sample database');
+
+$result = mysql_query("SELECT * from user", $connection)
+                                     or die ('Invalid query: '. mysql_error());
+
+while($row = mysql_fetch_assoc($result)) {
+    echo $row['firstname'] . "\n";
+}
+
+mysql_close($connection);
+?>
