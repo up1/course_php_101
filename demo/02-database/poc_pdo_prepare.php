d6b0c70b8ab85f4dde2f3408944c4ac1bfdd92e8
somkiat <somkiat.p@gmail.com>
2015-09-24 12:14:48 +0700

    Add demo of PDO with prepare statement

---
+++ demo/02-database/poc_pdo_prepare.php
@@ -0,0 +1,19 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "mypassword";
+$database_name = "sample";
+
+$connection = new PDO("mysql:host=".$host."; dbname=".$database_name."", 
+                                        $username, $password,
+                                        array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+
+$sql = 'SELECT * FROM USER WHERE id = :user_id';
+$statement = $connection->prepare($sql);
+$statement->execute(array(':user_id' => 1));
+
+$result = $statement->fetchAll();
+foreach ($result as $row) {
+    echo $row['firstname'] . "\n";
+}
+?>
